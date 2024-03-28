29e97af1ecc5412cdf81a19224ea35be8657089e
tina262317 <144104039+tina262317@users.noreply.github.com>
2024-03-28 12:28:19 +0100

    Someren Main project upload

---
+++ SomerenModel/Room.cs
@@ -0,0 +1,10 @@
+﻿namespace SomerenModel
+{
+    public class Room
+    {
+        public int Id { get; set; }         // database id
+        public int Number { get; set; }     // RoomNumber, e.g. 206
+        public int Capacity { get; set; }   // number of beds, either 4, 6, 8, 12 or 16
+        public bool Type { get; set; }      // student = false, teacher = true
+    }
+}
